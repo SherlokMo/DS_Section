2e95cc62e67919c7e9436406b2cef02964e79598
realMoSalah <redmohammad22@gmail.com>
2021-05-24 20:23:34 +0200

    LinkedList

---
+++ LinkedList/Adding.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Datastructure_task.LinkedList
+{
+    class Adding
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> L = new LinkedList<int>();
+            L.AddFirst(5); // {5}
+            L.AddFirst(2); // {2,5}
+            L.AddFirst(8); // {8,2,5}
+            L.AddLast(4);  // {8,2,5,4}
+            L.AddLast(9);  // {8,2,5,4,9}
+            L.AddLast(1);  // {8,2,5,4,9,1}
+
+            Console.Write("Linked List elements are: ");
+            foreach (int i in L)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ LinkedList/Search.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Datastructure_task.LinkedList
+{
+    class Search
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(4); // {4}
+            list.AddLast(9); // {4,9}
+            list.AddLast(1); // {4,9,1}
+            list.AddLast(3); // {4,9,1,3}
+            list.AddLast(6); // {4,9,1,3,6}
+            list.AddLast(2); // {4,9,1,3,6,2}
+
+            Console.WriteLine("The value 3 is present in Linked List: " + list.Contains(3)); // true
+            Console.WriteLine("The value 5 is present in Linked List: " + list.Contains(5)); // false
+
+        }
+    }
+}
